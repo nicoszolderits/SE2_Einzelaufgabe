446b5c852e470f5551a4675887178d4ecbca89ab
nicoszolderits <nszolderits@gmail.com>
2024-03-11 17:34:02 +0100

    Setup Server connection with sendToServer and connectSocket methods

--- app/src/main/java/com/example/se2_einzelaufgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelaufgabe/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +23,43 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Info",sendButton.getText()+" Button gedrückt");
+                Log.d("Info", sendButton.getText() + " Button gedrückt");
+
+                Thread networkThread = new Thread() {
+                    @Override
+                    public void run() {
+                        sendToServer();
+                    }
+                };
+
+                Log.d("Info", "Thread wurde erstellt");
+                networkThread.start();
+                Log.d("Info", "Thread wurde gestartet");
             }
         });
+    }
 
+    private void sendToServer() {
+        try {
+            Socket socket = connectSocket();
+
+            if (socket == null || !socket.isConnected()) {
+                throw new Exception("Fehler beim Verbinden.");
+            }
+        } catch (Exception exc) {
+            Log.d("Error", "MainActivity.createconnection" + exc.getMessage());
+        }
+    }
+
+    private Socket connectSocket() {
+        Socket socket;
+        try {
+            socket = new Socket("se2-submission.aau.at", 20080);
+            Log.d("Info", "Connected to se2-submission.aau.at.");
+            return socket;
+        } catch (Exception exc) {
+            Log.d("Error", "MainActivity.connectSocket" + exc.getMessage());
+        }
+        return null;
     }
 }
